09e8e47a272ec04e00e7b2fbd32c811d0b5b5441
johnny-walker <albert_hsu@cyberlink.com>
2021-06-12 23:06:02 +0800

    update Root.py

--- Root.py
+++ Root.py
@@ -11,8 +11,8 @@ class ProgramBase(tk.Frame):
         # configure window
         root.width = width
         root.height = height
-        geometry = '{0:d}x{1:d}'.format(root.width+5, root.height+5) 
-        root.geometry(geometry)    # root.geometry('605x405')
+        geometry = '{0:d}x{1:d}'.format(root.width, root.height) 
+        root.geometry(geometry)    # ex. root.geometry('600x400')
         root.title("window")
 
         # bind events
