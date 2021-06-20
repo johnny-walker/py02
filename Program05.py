c802ccb41fdf129786b8c96ad4b9ff36d7e9f257
johnny-walker <albert_hsu@cyberlink.com>
2021-06-20 11:26:23 +0800

    add MazeThread to move mouse

--- Program05.py
+++ Program05.py
@@ -5,29 +5,85 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 import cv2
 import numpy as np
-
 from Root import ProgramBase
 
+import threading
+
+THREAD_MOUSE_ID = 1    
+
+class MazeThread (threading.Thread):
+    def __init__(self, threadID, name, owner):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.owner  = owner       
+
+    def run(self):
+        print("Starting " + self.name + " id= " +str(self.threadID))
+        
+        if self.threadID == THREAD_MOUSE_ID :
+            self.owner.move(self.name)
+
 class Pgm05(ProgramBase):
+    threadEventMouse = threading.Event()
+    threadMouse = None
+    
     def __init__(self, root, path, width=640, height=480):
         super().__init__(root, width, height)
         self.root.title('Canvas')
 
         self.canvas = tk.Canvas(self.root, bg = "gray", width=width, height=height)
         self.canvas.pack()
-
-        img = Image.open('data/mouse.png')
-        img = img.resize((img.width//20,img.height//20), Image.ANTIALIAS)
-        self.img = self.loadImage('data/mouse.png')
-        self.canvas.create_image(10, 10, anchor = 'nw', image = self.img)
+        self.imgCV2 = None
+        self.imgTK = self.loadImage('data/mouse.png')
+        self.mouseImgID = self.canvas.create_image(100, 100, anchor = 'nw', image = self.imgTK)
         self.root.update()
     
     def loadImage(self, path):
         img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-        img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
-        im = Image.fromarray(img)
-        im.thumbnail((im.width//32, im.height//32))
-        return ImageTk.PhotoImage(im)
+        self.imgCV2 = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+        self.rotateImage(90, 0.8)
+        return self.resizeAsTKImg()
+
+    def resizeAsTKImg(self):
+        im = Image.fromarray(self.imgCV2)               # convert to pillow image fomrat
+        im.thumbnail((im.width//12, im.height//12))     # resize by pillow
+        return ImageTk.PhotoImage(im)                   # convert to tkinter image
+
+    def rotateImage(self, angle, scale=1.0):
+        rows, cols = self.imgCV2.shape[:2]
+        matrix2D = cv2.getRotationMatrix2D(((cols-1)/2.0, (rows-1)/2.0), angle, scale)
+        self.imgCV2 = cv2.warpAffine(self.imgCV2, matrix2D, (cols,rows))
+
+    def updateMouseImage(self):
+        self.imageTKMouse = self.resizeAsTKImg()
+        self.canvas.itemconfig(self.mouseImgID, image=self.imageTKMouse)
+
+    def onKey(self, event):
+        if event.char == event.keysym or len(event.char) == 1:
+            if event.keysym == 'Escape':
+                self.threadEventMouse.set() # signal the thread loop to quit
+                print("key Escape") 
+                self.root.destroy()
+            else: # any other key
+                self.startWalking()
+    
+    def startWalking(self):
+        print ('start moving')
+        self.threadEventMouse.clear()   # reset the thread event
+    
+        # create thread
+        self.threadMouse = MazeThread(THREAD_MOUSE_ID, "Mouse Thread", self)
+        self.threadMouse.start()
+    
+    def move(self, threadName):
+        while True:
+            if self.threadEventMouse.wait(0.1):  # moving for every 100 ms
+                break
+            print ('[{0}] keep moving'.format(threadName))
+            self.rotateImage(10, 1.0)
+            self.updateMouseImage()
+        print('[{0}] exit'.format(threadName))
 
 if __name__ == '__main__':
     cwd = os.getcwd()
